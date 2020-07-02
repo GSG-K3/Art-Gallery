b3fecc333d09478e8c2bba4b2cf43d8f0e1d25c9
bayan-404 <bayan.jubah@gmail.com>
2020-07-02 16:18:36 +0300

    db build

--- server/database/config/dbBuild.js
+++ server/database/config/dbBuild.js
@@ -1,1 +1,18 @@
-// we will write here code to build sql files 
+const fs = require('fs')
+const path = require('path')
+
+const dbConnection = require('./dbConnection')
+
+const sql = fs.readFileSync(path.join(__dirname, 'dbBuild.sql')).toString()
+const sql2 = fs.readFileSync(path.join(__dirname, 'dummyData.sql')).toString()
+
+const runDbBuild = (sql, sql2) => {
+  return dbConnection
+    .query(sql, sql2)
+    .then((res) => res)
+    .catch((err) => console.log(err))
+}
+
+runDbBuild(sql, sql2)
+
+module.exports = runDbBuild
