a3209d05e52ee5a858b8e21702aab2a4e50b8d82
bayan-404 <bayan.jubah@gmail.com>
2020-07-05 19:13:47 +0300

    add the endpoint for log in and run it

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const path = require('path')
 const express = require('express')
 const router = require('./controllers')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
 
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(router)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+// })
 
 module.exports = app
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const getArt = require('../controllers/getArt')
+const login = require('./logIn')
 
 router.get('/api/get-art', getArt)
-
+router.post('/api/login', login)
 module.exports = router
