8c0136714ff183151703a7c4c608cff56e4ba374
bayanseder <bayansider88@gmail.com>
2020-07-06 13:13:02 +0300

    delete disable power-by-x

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
 
 app.use(router)
-app.disable('x-powered-by')
 app.use(express.urlencoded({ extended: false }))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
