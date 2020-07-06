bd1644af7e8381c2cc65a18217cf94d8b0ab19ab
bayan-404 <bayan.jubah@gmail.com>
2020-07-06 23:30:07 +0300

    fix the code

--- server/controllers/logIn.js
+++ server/controllers/logIn.js
@@ -5,31 +5,40 @@ const login = (req, res) => {
   const { email, password } = req.body
 
   if (email.trim().length === 0 || password.trim().length === 0) {
-    res.status(400).json({
-      message: 'All Fields Are Requered !!!!!',
-      status: 400
-    })
-  } else {
-    getPassword(email)
-      .then((result) => {
-        if (!result || result.rows.length === 0) {
-          res.status(500).json({
-            message: 'Email Does Not Exist !!!!',
-            status: 500
-          })
-        }
-        const token = createToken(result, password, email)
-        console.log(token)
-        if (token) {
-          res
-            .cookie('token', token, { maxAge: 900000, httpOnly: true })
-            .status(200)
-            .json({ message: 'you are loged in successfully!!!!' })
-        }
-      }
-      )
-      .catch((err) => console.log(err))
+    return res.status(400).clearCookie('token')
+      .json({
+        message: 'All Fields Are Requered !!!!!',
+        status: 400
+      })
   }
+
+  getPassword(email)
+    .then(async (result) => {
+      if (result.rowCount === 0) {
+        return res.status(403).clearCookie('token').json({
+          message: 'Email or passowrd is not correct !!!!',
+          status: 403
+        })
+      }
+      const token = await createToken(result.rows[0], password)
+      if (!token) {
+        res.status(403).clearCookie('token').json({
+          message: 'Login faild Check your email or passowrd',
+          status: 403
+        })
+      }
+      return res
+        .cookie('token', token, { maxAge: 900000, httpOnly: true })
+        .status(200)
+        .json({ message: 'you are loged in successfully!!!!' })
+    }
+    )
+    .catch((err) => {
+      return res.status(501).clearCookie('token').json({
+        message: 'internal error , login faild',
+        status: 501
+      })
+    })
 }
 
 module.exports = login
