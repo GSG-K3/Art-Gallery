c778b568300dd96fe825de2d58158694a73efeae
bayan-404 <bayan.jubah@gmail.com>
2020-07-06 12:01:57 +0300

    uncommenting

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(router)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+})
 
 module.exports = app
