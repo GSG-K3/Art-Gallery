327cf1e60f5f9627045d3d1462ecdf53e4ef3ded
bayanseder <bayansider88@gmail.com>
2020-07-06 13:17:36 +0300

    adding art backend #44

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const getArt = require('../controllers/getArt')
-
+const getArt = require('./getArt')
+const signup = require('./signup')
+const checkEmail = require('../middlewares/checkEmail')
+const addArt = require('./postArt')
 router.get('/api/get-art', getArt)
+router.post('/api/signup', checkEmail, signup)
+router.post('/api/add-art', addArt)
 
 module.exports = router
