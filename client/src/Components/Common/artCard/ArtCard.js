cd9387292ac349accfbb6b6160f126cdd67df815
aboodswiti <swaiti393@gmail.com>
2020-07-08 19:24:59 +0300

    replace class with Hooks and add comments

--- client/src/Components/Common/artCard/ArtCard.js
+++ client/src/Components/Common/artCard/ArtCard.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 190 ,
+    maxWidth: 190,
   },
 });
 
 export default function ArtCard(props) {
   const classes = useStyles();
-  const {
-    title,
-    imageurl,
-    artist,
-  } = props;
+  const { title, imageurl, artist } = props;
 
   return (
     <Card className={classes.root}>
--- client/src/Components/Common/homePage/Home.js
+++ client/src/Components/Common/homePage/Home.js
@@ -1,65 +1,25 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid, Box } from '@material-ui/core';
-import ArtCard from '../artCard/ArtCard';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Box } from "@material-ui/core";
+import ArtCard from "../artCard/ArtCard";
 
-
-const useStyles = (theme) => ({
-    root: {
-      [theme.breakpoints.between('0', '400')]: {
-        width: '310px !important',
-      },
-    },
-  });
-  
-
-class Home extends Component {
-    state = {
-        data : [
-            {title: 'Dog', artist: "somaya", imageurl:'https://imgc.allpostersimages.com/img/print/u-g-F13PZJ0.jpg?w=550&h=550&p=0'},
-            {title: 'Cat', artist: "Beno", imageurl:'https://cdn.shopify.com/s/files/1/1039/2274/products/lips_large.jpeg?v=1452114778'},
-            {title: 'Cat', artist: "Beno", imageurl:'https://i.pinimg.com/236x/ca/1e/24/ca1e24af92e70d2db2ea993634236ca8--dog-artwork-pet-art.jpg'},
-            {title: 'horse', artist: "Bebo", imageurl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdzvrd_My_DiEtzSanE5g6J368qFf7Lc-JMA&usqp=CAU'},
-            {title: 'Dog', artist: "somaya", imageurl:'https://imgc.allpostersimages.com/img/print/u-g-F13PZJ0.jpg?w=550&h=550&p=0'},
-            {title: 'Cat', artist: "Beno", imageurl:'https://cdn.shopify.com/s/files/1/1039/2274/products/lips_large.jpeg?v=1452114778'},
-            {title: 'Cat', artist: "Beno", imageurl:'https://i.pinimg.com/236x/ca/1e/24/ca1e24af92e70d2db2ea993634236ca8--dog-artwork-pet-art.jpg'},
-            {title: 'horse', artist: "Bebo", imageurl:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdzvrd_My_DiEtzSanE5g6J368qFf7Lc-JMA&usqp=CAU'}
-          ]    };
-    
-
-    render() {
-      const { classes } = this.props;
-      const { data } = this.state;
-      return (
-
-        <Grid  container direction="row" spacing={2} justify='space-around' >
-
+function Home() {
+  return (
+    <Grid container direction="row" spacing={2} justify="space-around">
       {data.map((info) => {
-
-            return (
-
-  <Grid item xs={6} sm={4} lg	={3}>
-
-        <ArtCard
+        // data : its the data from data base  {title , artist, imageurl }.
+        return (
+          <Grid item xs={6} sm={4} lg={3}>
+            <ArtCard
               title={info.title}
               artist={info.artist}
-              imageurl={info.imageurl}/>
-      </Grid> )
-        }               
-                     
-)}
+              imageurl={info.imageurl}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
 
-</Grid>
-
-)
-
-  
-  }}
-  
-  export default withStyles(useStyles)(Home);
-  
-
-
-
-
-  
+export default Home;
