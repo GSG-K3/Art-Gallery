b2f3cf4eb71e2aabd63c4d6d01854350fade1105
bayan-404 <bayan.jubah@gmail.com>
2020-07-09 14:07:49 +0300

    resolve conflict!!!!

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Home from './Components/Common/homePage/Home'
 import CategorisSlider from './Components/Common/categoriesSlider/CategoriesSlider'
 import RecentArtSlider from './Components/Common/RecentArtSlider/RecentArtSlider'
+import Header from './Components/Common/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
+      <RecentArtSlider />    
+      <Home/>
       <CategorisSlider/>
     </div>
   );
