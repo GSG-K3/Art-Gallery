b51f1da0649dc6723829c69063619df74762d8c1
bayan-404 <bayan.jubah@gmail.com>
2020-07-14 00:34:56 +0300

    call the fileUpload

--- server/app.js
+++ server/app.js
@@ -2,15 +2,16 @@ const path = require('path')
 const express = require('express')
 const router = require('./controllers')
 const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
-
+app.use(fileUpload())
 app.use(express.urlencoded({ extended: false }))
+
 app.use(cookieParser())
 app.use(router)
-app.use(express.urlencoded({ extended: false }))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 })
