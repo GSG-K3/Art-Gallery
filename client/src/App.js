7153d047aa8308924a467d7ba3d48ba02d9095e0
aboodswiti <swaiti393@gmail.com>
2020-07-14 04:30:53 +0300

    set login and signUp end-point

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Components/Common/homePage/Home'
 import Login from './Components/Layouts/Login/Login'
+import Signup from './Components/Layouts/signUp/SignUp'
 import RecentArtSlider from './Components/Common/RecentArtSlider/RecentArtSlider'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={RecentArtSlider} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
 
       </Router>
