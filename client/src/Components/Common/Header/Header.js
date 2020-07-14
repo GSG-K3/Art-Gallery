57985a7576276f52fdcd28c6503f3ae423c674bf
Duha96 <141046@ppu.edu.ps>
2020-07-14 16:41:47 +0300

    add edit icon to header

--- client/src/Components/Common/Header/Header.js
+++ client/src/Components/Common/Header/Header.js
@@ -4,7 +4,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-
+import EditIcon from '@material-ui/icons/Edit';
+import Profile from '../../Layouts/ProfileArtist/profile';
 
 
 const Header = (props) => {
@@ -20,7 +21,12 @@ return(
             <IconButton edge="start" className={classes.shopButton} color="#DADADA" aria-label="shop">
             <ShoppingCartIcon />
           </IconButton>
-
+          {pageName==="Your Profile"?
+          <IconButton edge="start" className={classes.editButton}   edge="start" color="#DADADA" aria-label="edit">
+            <EditIcon/>
+          </IconButton> 
+          :null
+          }
           
         </Toolbar>
     </div>
