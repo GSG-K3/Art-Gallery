fa294faec557c64db2628787acd8827a5e18cbb3
bayan-404 <bayan.jubah@gmail.com>
2020-07-15 10:23:18 +0300

    fix the code to take a url from backend

--- client/src/Components/Common/UplodeImage/uploadImage.js
+++ client/src/Components/Common/UplodeImage/uploadImage.js
@@ -1,55 +1,86 @@
-import React ,{Fragment , useState} from 'react'
-import AddIcon from '@material-ui/icons/Add';
+import React, { Fragment, useState } from 'react'
+import AddIcon from '@material-ui/icons/Add'
+// import RaisedButton from 'material-ui/core/RaisedButton';
+import { Button } from '@material-ui/core'
+import IconButton from '@material-ui/core/IconButton'
 import axios from 'axios'
+import useStyles from './uploadImageStyle'
+import {Box} from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 
+const UploadImage = ({getImageUrl}) => {
+  const classes = useStyles()
+  const [image, addImage] = useState('')
+  const [imageName, addImageName] = useState('choose file')
+  const [uploadedImage, setUploadedImage] = useState('')
 
-const UploadImage = () =>{
-  const [image , addImage] = useState('')
-  const [imageName , addImageName] = useState('choose file')
-  const [uploadedImage , setUploadedImage] = useState('')
-
-  const handaleImage =async (e) =>{
+  const handaleImage = async (e) => {
     addImage(e.target.files[0])
     addImageName(e.target.files[0].name)
     console.log(e.target.files[0])
     //for backend it's a js thing it construct a set of key and value representing form fields and their values
-    const formData = new FormData();
-    formData.append('Image' , e.target.files[0])
-       try {
-      const res = await axios.post('/api/uploadFile',formData , {
+    const formData = new FormData()
+    formData.append('Image', e.target.files[0])
+    try {
+      const res = await axios.post('/api/upload', formData, {
         headers: {
-          'content-Type' : 'multipart/form-data'
-        }
-      });
+          'content-Type': 'multipart/form-data',
+        },
+      })
 
-      const {fileName , filePath} = res.data;
+      const { url } = res.data
 
-      setUploadedImage (fileName)
-      console.log(filePath,fileName)
-    }catch(err){
-      if(err.response.status === 500){
+      setUploadedImage(url)
+      if (getImageUrl){
+        getImageUrl(url)
+      }
+      console.log(url)
+    } catch (err) {
+      if (err.response.status === 500) {
         console.log('there was a problem with the server')
-      }else{
+      } else {
         console.log(err.response.data.msg)
       }
     }
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
   }
-  
-  return (
-    <Fragment>
-    <form onSubmit = {handleSubmit}>
-      <AddIcon color = 'primary' style={{ fontSize: 100 }} > add picture</AddIcon>
-      <h2 > add picture </h2>
-      <input type="file" id="img" name="img" accept="image/*" onChange = {handaleImage}/>
-      <lable>{imageName}</lable>
-      <input type = "submit"/>
-      </form>
-      <img src = {`/upload/${uploadedImage}`}/>
 
+  const displayStatus = !uploadedImage ? 'none' : 'block';
+  return (
+    <Fragment className={classes.root}>
+      <form onSubmit={handleSubmit} className = {classes.uploadContaner}>
+        <input
+          className={classes.input}
+          type="file"
+          id="icon-button-file"
+          name="img"
+          accept="image/*"
+          onChange={handaleImage}
+        />
+        <Grid alignItems = 'center' justify = 'center' item direction = 'column' containe className = {classes.iconBox}>
+        <label htmlFor="icon-button-file">
+          <IconButton
+            aria-label="upload picture"
+            component="span"
+            color="primary"
+          >
+            <AddIcon className={classes.icon} />
+          </IconButton>
+        </label>
+        <lable>{imageName}</lable>
+          </Grid>
+        <Button className = {classes.input} type="submit" label="submit" color="primary">
+          submit
+        </Button>
+      </form>
+      <Box component = 'div' display = {displayStatus}>
+      <Grid alignItems = 'center' justify = 'center' item container>
+      <img className = {classes.theImage} src={uploadedImage} />
+      </Grid>
+      </Box>
     </Fragment>
   )
 }
