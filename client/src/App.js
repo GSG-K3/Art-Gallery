c2292cf5ab2f3bd6ef0fe0b362fd439cd39b4cf0
Duha96 <141046@ppu.edu.ps>
2020-07-15 17:03:16 +0300

    add id for the profile path

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './Components/Layouts/HomePage/Home'
+import Profile from './Components/Layouts/ProfileArtist/profile'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ArtInfo from './Components/Layouts/ArtInfo/ArtInfo'
 
@@ -8,6 +9,7 @@ const App = ()=> {
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/profile/:id' component={Profile} />
         <Route exact path='/art/:id' component={ArtInfo} />
       </Switch>
     </Router>  
