93177947afc4e7ad03d425429be57d0e1f934d6c
bayan-404 <bayan.jubah@gmail.com>
2020-07-15 10:24:25 +0300

    modify the code to take the url through props

--- client/src/Components/Layouts/addArt/addArt.js
+++ client/src/Components/Layouts/addArt/addArt.js
@@ -1,30 +1,39 @@
-import React ,{Fragment , useState} from 'react'
-import { TextField } from '@material-ui/core';
+import React, { Fragment, useState } from 'react'
+import { TextField } from '@material-ui/core'
 import axios from 'axios'
 import UploadedImage from '../../Common/UplodeImage/uploadImage'
+import useStyle from './addArtStyle'
 
-
-const AddArt = () =>{
+const AddArt = () => {
+  const classes = useStyle()
+  const [imageUrl , setImageUrl] = useState(null)
+  const getUrl = (url) =>{
+    console.log(url , 'url from addArt')
+   setImageUrl(url)
+  }
   return (
-    <div>
-    <UploadedImage/>
-    <form >
-     <TextField
+    <div className={classes.root}>
+      <UploadedImage getImageUrl = {getUrl}/>
+      <form className={classes.inputsContaner}>
+        <TextField
+          className={classes.singleInput}
           id="outlined-helperText"
           label="art name"
           variant="outlined"
         />
-    <TextField
+        <TextField
+          className={classes.singleInput}
           id="outlined-helperText"
           label="description"
           variant="outlined"
         />
-    <TextField
+        <TextField
+          className={classes.singleInput}
           id="outlined-helperText"
           label="categories"
           variant="outlined"
         />
-    </form>
+      </form>
     </div>
   )
 }
