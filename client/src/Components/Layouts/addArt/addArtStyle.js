d99c84c4d3880bbbe9462f1de43b5f165e2b3619
bayan-404 <bayan.jubah@gmail.com>
2020-07-15 10:29:39 +0300

    add style

---
+++ client/src/Components/Layouts/addArt/addArtStyle.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles ((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  inputsContaner : {
+    display : 'flex',
+    flexDirection : 'column',
+    alignItems :'center'
+  },
+  singleInput : {
+    padding : 5 
+  }
+}))
+
+export default useStyle
