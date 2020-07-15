47f2df3352d38f53827f71068fcf575bd6fae902
bayan-404 <bayan.jubah@gmail.com>
2020-07-15 10:25:27 +0300

    modify the path

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const addArt = require('./postArt')
-const uploadFile = require('./uploadFile')
 const login = require('./logIn')
 const getArt = require('./getArt')
 const signup = require('./signup')
 const checkEmail = require('../middlewares/checkEmail')
 const recentArt = require('./recentArt')
 const getArtAndUser = require('./getArtAndUser')
+const uploadImage = require('./uplodeImage')
 
 router.get('/api/get-art', getArt)
 router.post('/api/signup', checkEmail, signup)
 router.post('/api/login', login)
 router.post('/api/add-art', addArt)
-router.post('/api/uploadFile', uploadFile)
 router.get('/api/recent', recentArt)
 router.get('/api/art-user/:id', getArtAndUser)
+router.post('/api/upload', uploadImage)
 
 module.exports = router
