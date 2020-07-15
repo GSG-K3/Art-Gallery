f3d3b312c2da3f8f3f1bb1358e181e96d91c3459
bayan-404 <bayan.jubah@gmail.com>
2020-07-15 10:25:58 +0300

    nothing much

--- server/app.js
+++ server/app.js
@@ -3,17 +3,26 @@ const express = require('express')
 const router = require('./controllers')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
+})
 
 const app = express()
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 app.use(express.json())
-app.use(fileUpload())
+app.use(fileUpload({
+  useTempFiles: true
+}))
 app.use(express.urlencoded({ extended: false }))
 
 app.use(cookieParser())
 app.use(router)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+// })
 
 module.exports = app
