3743e3033c3c2c08c45e4fb33099cecd5a75c3c7
bayan-404 <bayan.jubah@gmail.com>
2020-07-18 11:47:07 +0300

    send the data to the controller

--- server/controllers/postArt.js
+++ server/controllers/postArt.js
@@ -2,7 +2,9 @@ const addArt = require('../database/queries/addArtQuery')
 
 const postArt = (req, res) => {
   const data = req.body
-  addArt(data)
+  const user_id = 1
+  console.log('hi Im in the controller', req.body)
+  addArt(user_id, data)
     .then(result => res.status(200).json({ message: 'successfully added' }))
     .catch(err => res.status(403).json({ error: err.message }))
 }
