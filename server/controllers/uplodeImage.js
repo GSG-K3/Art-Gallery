f7cdb28991cd85e9af1c6954e618072ed51626cc
bayan-404 <bayan.jubah@gmail.com>
2020-07-18 11:49:17 +0300

    remove console and install material lab

--- server/controllers/uplodeImage.js
+++ server/controllers/uplodeImage.js
@@ -1,10 +1,8 @@
 const cloudinary = require('cloudinary').v2
 const uploadImage = (req, res) => {
   const file = req.files.Image
-  console.log(req.files)
   cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
     console.log('Error:', err)
-    console.log('Result', result)
     if (err) {
       return res.status(400).json({ msg: 'error in uploading image', err })
     }
