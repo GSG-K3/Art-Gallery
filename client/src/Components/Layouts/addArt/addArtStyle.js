89bb5ef958aa04f504a3acb072e933f848caa14b
bayan-404 <bayan.jubah@gmail.com>
2020-07-18 11:45:38 +0300

    style for components

--- client/src/Components/Layouts/addArt/addArtStyle.js
+++ client/src/Components/Layouts/addArt/addArtStyle.js
@@ -1,19 +1,20 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyle = makeStyles ((theme) => ({
+const useStyle = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
     },
   },
-  inputsContaner : {
-    display : 'flex',
-    flexDirection : 'column',
-    alignItems :'center'
+  inputsContaner: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  singleInput : {
-    padding : 5 
-  }
-}))
+  singleInput: {
+    padding: 5,
+    width : 300
+  },
+}));
 
-export default useStyle
+export default useStyle;
