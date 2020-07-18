71976a644a264e5e826bcdd9d7d4c7fa18ff3cc4
bayan-404 <bayan.jubah@gmail.com>
2020-07-18 11:44:20 +0300

    add new components

--- server/database/queries/addArtQuery.js
+++ server/database/queries/addArtQuery.js
@@ -1,9 +1,9 @@
 const dbConnection = require('../config/dbConnection')
 
-const artPost = (data) => {
+const artPost = (user_id, data) => {
   const sql = {
-    text: 'INSERT INTO artwork (titel ,artist_id ,photo_url ,description ,price ,category ,size ,type) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)',
-    values: [data.name, data.id, data.photo, data.description, data.price, data.category, data.size, data.type]
+    text: 'INSERT INTO artwork (titel ,artist_id ,photo_url ,description ,price ,category ,size ,type) VALUES ($1,$2,$3,$4,$5,$6,$7,$8 ,$9)',
+    values: [data.name, user_id, data.id, data.photo, data.description, data.price, data.category, data.size, data.type]
   }
   return dbConnection.query(sql)
 }
