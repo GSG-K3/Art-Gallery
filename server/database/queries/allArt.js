f5ed0b6db7fa428cb8b26c54f5fb32a8afc07d12
Duha96 <141046@ppu.edu.ps>
2020-07-20 14:16:08 +0300

    delete allArt file

--- server/database/queries/allArt.js
+++
@@ -1,9 +0,0 @@
-const dbConnection = require('../config/dbConnection')
-
-const  getArtQueries = () => {
-  console.log('im in the quere' );
-  const sql = 'SELECT * FROM artwork  ';
-  console.log(sql);
-  return dbConnection.query(sql) 
-}
-module.exports = getArtQueries;
