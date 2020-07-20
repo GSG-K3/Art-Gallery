70414109a09836062dec9d044cf68da38d83759f
aboodswiti <swaiti393@gmail.com>
2020-07-20 15:51:36 +0300

    add livePreview & customize endPoint

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import ArtInfo from './Components/Layouts/ArtInfo/ArtInfo';
 import Home from './Components/Layouts/HomePage/Home';
 import Login from './Components/Layouts/Login/Login';
 import Signup from './Components/Layouts/signUp/SignUp';
+import Customize from './Components/Layouts/Customize/customize';
+import UpladImage from './Components/Layouts/Customize/UploadImage';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/art/:id' component={ArtInfo} />
+        <Route exact path='/customize' component={Customize} />
+        <Route exact path='/livePreview' component={UpladImage} />
       </Switch>
     </Router>
   );
