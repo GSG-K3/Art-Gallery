f373308a59ec05f50587d98d43dc405d30aa8f1a
Duha96 <141046@ppu.edu.ps>
2020-07-21 18:10:29 +0300

    make id variables

--- client/src/Components/Layouts/ProfileArtist/profile.js
+++ client/src/Components/Layouts/ProfileArtist/profile.js
@@ -8,32 +8,69 @@ import ProfileHeader from './profileH'
 import AddButton from './addButton'
 import Tabs from './tab'
 import Header from '../../Common/Header/Header'
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
 
 function Profile() {
   const classes = useStyles();
   const [artistValue, setArtistValue] = useState(null);
+  const [clientId,setId] = useState(null);
+  const history = useHistory()
   useEffect(() => {
-    if (artistValue) {
-      return;
-    }
-    axios
-      .get('/api/artist/4')
-      .then((result) =>  setArtistValue(result.data))
-      .then ((result) =>  result)
-      .catch((err) => err);
+    axios.get('/api/user-id')
+        .then(result=> {
+          if(!result.data.success){
+           history.push('/login')
+          } 
+
+          else { 
+            if(result.data.role === 'client'){
+              history.push('/cart')
+            }
+         
+
+                    if (!artistValue)  
+                    { 
+                      setId(result.data.id)
+                      axios
+                      .get(`/api/artist/${result.data.id}`)
+                      .then((result) =>  setArtistValue(result.data))
+                       
+                      .catch((err) => err);
+
+
+                    }
+                    }
+   
+    
+  
+    }) 
+  
+    .catch(err=>swal({
+      title: 'حدث خطأ في الخادم',
+      icon: 'warning',
+    }))
+
   }, [artistValue]);
   
   
   return (
+    
     <Grid container direction='column' className={classes.root}>
+      
+      {artistValue?
+     <div>
+      
       <Header pageName='Your Profile'  />
       {artistValue? 
       <ProfileHeader  pageName='Your Profile'  artistName={artistValue}/>
     : null}
-      <Tabs artistName={artistValue}/>
+      <Tabs  artistName = {artistValue}  />
       <AddButton/>
-      
+      </div>
+    : null }  
       <Navbar/>
+      
       
     </Grid>
   );
