6b3ed8461bf8e7717e02b6d4a7f10b99d256796b
bayan-404 <bayan.jubah@gmail.com>
2020-07-21 12:21:50 +0300

    add art form to the database with the id fixed

--- server/database/queries/addArtQuery.js
+++ server/database/queries/addArtQuery.js
@@ -1,7 +1,6 @@
 const dbConnection = require('../config/dbConnection')
 
 const artPost = (data) => {
-  console.log(data, 'queryyyyyyyyyyyyyyyyyy')
   const sql = {
     text: 'INSERT INTO artwork (title ,artist_id ,photo_url ,description ,price ,category ,size ,type,sold) VALUES ($1,$2,$3,$4,$5,$6,$7,$8 ,$9 )',
     values: [data.name, data.user_id, data.photo, data.description, data.price, data.category, data.size, data.type, data.sold]
