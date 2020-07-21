a0aeb99d7c29c23f1aa05af6ba6e1157378ac6af
bayan-404 <bayan.jubah@gmail.com>
2020-07-21 12:20:59 +0300

    fix the query to post form data

--- server/controllers/postArt.js
+++ server/controllers/postArt.js
@@ -4,9 +4,7 @@ const postArt = (req, res) => {
   const data = req.body
   addArt(data)
     .then(result => res.status(200).json({ message: 'successfully added' }))
-    .catch(err =>
-      //  res.status(403).json({ error: err.message })
-      console.log(err))
+    .catch(err => res.status(403).json({ error: err.message }))
 }
 
 module.exports = postArt
