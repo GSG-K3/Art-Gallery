a65420c759253f49f37dea0414a743e65f06a3da
Duha96 <141046@ppu.edu.ps>
2020-07-22 15:47:37 +0300

    redirect user when click profile icon

--- client/src/Components/Common/Navbar/Navbar.js
+++ client/src/Components/Common/Navbar/Navbar.js
@@ -5,37 +5,12 @@ import PersonIcon from '@material-ui/icons/Person';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HomeIcon from '@material-ui/icons/Home';
-import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-
 
 const NavBar = () => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-  const [login, setlogin] = React.useState(false);
-
   const history = useHistory();
-
-  const CheckLogin = () => {
-
-    axios.get('/api/user-id')
-      .then(result => {
-        if (!result.data.success) {
-
-          history.push('/login')
-        }
-
-        else {
-          if (result.data.role === 'client') {
-            return history.push('/cart')
-          }
-          return history.push(`/profile/${result.data.id}`)
-        }
-
-
-      })
-      .catch()
-  }
 
   return (
     <div>
@@ -62,14 +37,11 @@ const NavBar = () => {
         <BottomNavigationAction
           className={classes.iconClass}
           label='You'
-          onClick={CheckLogin}
+          onClick={() => history.push('/login')}
           icon={<PersonIcon />}
         />
       </BottomNavigation>
-
     </div>
-
-
   );
 };
 export default NavBar;
