f2890221ab6a631137438502bf2372b96097e035
Duha96 <141046@ppu.edu.ps>
2020-07-22 16:01:33 +0300

    make artwork as props form profile

--- client/src/Components/Layouts/ProfileArtist/allArt.js
+++ client/src/Components/Layouts/ProfileArtist/allArt.js
@@ -1,52 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import useStyles from './style';
 import ArtCard from '../../Common/ArtCard/ArtCard';
-import axios from 'axios';
 import { Grid } from '@material-ui/core';
-import CircularProgress from '@material-ui/core/CircularProgress'
+import CircularProgress from '@material-ui/core/CircularProgress';
 import green from '@material-ui/core/colors/green';
 
-
- const AllArt = (props) =>{
+const AllArt = (props) => {
   const classes = useStyles();
 
-  const {artwork} = props;
+  const { artwork } = props;
 
-  // useEffect(() => {
-  //   if (artwork) {
-  //     return;
-  //   }
-   
-  //   axios
-  //     .get(`/api/all-art-artist/${id}`)
-  //     .then((result) =>  setArtwork(result.data))
-  //     .catch((err) => err);
-  // }, [artwork]);
-  
   return (
     <div>
-      
       <Grid container direction='row' spacing={2} justify='space-around'>
-        {artwork
-          ? artwork.map((art) => {
-              return (
-
-                <Grid item xs={6}>
-                  <ArtCard
-                    title={art.titel}
-                    category={art.category}
-                    imageurl={art.photo_url}
-                    cardKey={art.id}
-                  />
-                </Grid>
-              );
-            })
-          : <div className={classes.root} style={{display:'flex', justifyContent:'center'}}>
-          <CircularProgress  style={{ color: green[400] }} />
-        </div>}
+        {artwork ? (
+          artwork.map((art) => {
+            return (
+              <Grid item xs={6}>
+                <ArtCard
+                  title={art.titel}
+                  category={art.category}
+                  imageurl={art.photo_url}
+                  cardKey={art.id}
+                />
+              </Grid>
+            );
+          })
+        ) : (
+          <div
+            className={classes.root}
+            style={{ display: 'flex', justifyContent: 'center' }}
+          >
+            <CircularProgress style={{ color: green[400] }} />
+          </div>
+        )}
       </Grid>
     </div>
   );
-}
+};
 
 export default AllArt;
