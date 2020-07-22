e4e3c571ae12082125e74f005808fea3c1c23224
Duha96 <141046@ppu.edu.ps>
2020-07-22 15:51:08 +0300

    add link to the artist name to redirect user to artist profile

--- client/src/Components/Layouts/ArtInfo/ArtInfo.js
+++ client/src/Components/Layouts/ArtInfo/ArtInfo.js
@@ -12,12 +12,13 @@ import green from '@material-ui/core/colors/green';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import SecondHeader from '../../Common/SecondHeder/SecondHeader';
 import swal from 'sweetalert';
+import { Link } from 'react-router-dom';
 
 const ArtInfo = () => {
   const classes = useStyles();
   const [artUser, setArtUser] = useState(null);
   const [errorFound, setError] = useState(null);
-  let userId = null
+  let userId = null;
   const artId = window.location.pathname.slice(5, 10);
   useEffect(() => {
     if (artUser) {
@@ -30,34 +31,35 @@ const ArtInfo = () => {
   }, [artUser]);
 
   const addToCart = (artId) => {
-    axios.get('/api/user-id')
-          .then(result=> {
-            if(!result.data.success){
-              swal({
-                title: "الرجاء تسجيل الدخول لاتمام العملية",
-                icon: "warning",
-              })
-              
-            }
-            else{
-              userId = result.data.id
-              axios
-              .post('/api/add-cart', {
-                user: userId,
-                artwork: artId,
-              })
-              .then((result) =>
-                swal('رائع !!', 'يمكنك الذهاب الى سلة مشترياتك للتحقق', 'success'),
-              )
-              .catch(
-                (err) => swal('حدث خطأ اثناء العمليه .. يرجى المحاوله مجددا'),
-                'warning',
-              );
-             
-          
-            }})
-            .catch(err => err)
-       
+    axios
+      .get('/api/user-id')
+      .then((result) => {
+        if (!result.data.success) {
+          swal({
+            title: 'الرجاء تسجيل الدخول لاتمام العملية',
+            icon: 'warning',
+          });
+        } else {
+          userId = result.data.id;
+          axios
+            .post('/api/add-cart', {
+              user: userId,
+              artwork: artId,
+            })
+            .then((result) =>
+              swal(
+                'رائع !!',
+                'يمكنك الذهاب الى سلة مشترياتك للتحقق',
+                'success',
+              ),
+            )
+            .catch(
+              (err) => swal('حدث خطأ اثناء العمليه .. يرجى المحاوله مجددا'),
+              'warning',
+            );
+        }
+      })
+      .catch((err) => err);
   };
   return (
     <Grid container className={classes.root} direction='column'>
@@ -75,44 +77,56 @@ const ArtInfo = () => {
             <Typography variant='h5' align='center'>
               {artUser[0].title}
             </Typography>
+
+            <Link
+              to={{
+                pathname: `/profile/${artUser[0].artist_id}`,
+                state: { idArtist: artUser[0].artist_id },
+              }}
+              className='text-link'
+            >
+              <Typography variant='h6' align='center'>
+                {artUser[0].name}
+              </Typography>
+            </Link>
             <Typography variant='h6' align='center'>
-              {artUser[0].name} (
+              (
               <StarRoundedIcon style={{ color: yellow[400] }} />{' '}
               {artUser[0].rate})
             </Typography>
           </div>
           <div className={classes.detailDiv}>
-          <div
-            className={classes.divs}
-            style={{
-              flexDirection: 'column',
-              justifyContent: 'flex-end',
-              marginTop: 20,
-            }}
-          >
-            <Typography variant='h6' align='right'>
-              الوصف
-            </Typography>
-            <Typography variant='body1' gutterBottom align='right'>
-              {artUser[0].description}
-            </Typography>
-          </div>
-          <div className={classes.divs}>
-            <Typography variant='body1' gutterBottom align='right'>
-              {artUser[0].size}
-            </Typography>
-            <Typography variant='h6' align='right'>
-              الحجم
-            </Typography>
-          </div>
-          <div className={classes.divs}>
-            <Typography variant='body1' gutterBottom align='right'>
-              {artUser[0].category}
-            </Typography>
-            <Typography variant='h6' align='right'>
-              الفئة
-            </Typography>
-          </div>
+            <div
+              className={classes.divs}
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+                marginTop: 20,
+              }}
+            >
+              <Typography variant='h6' align='right'>
+                الوصف
+              </Typography>
+              <Typography variant='body1' gutterBottom align='right'>
+                {artUser[0].description}
+              </Typography>
+            </div>
+            <div className={classes.divs}>
+              <Typography variant='body1' gutterBottom align='right'>
+                {artUser[0].size}
+              </Typography>
+              <Typography variant='h6' align='right'>
+                الحجم
+              </Typography>
+            </div>
+            <div className={classes.divs}>
+              <Typography variant='body1' gutterBottom align='right'>
+                {artUser[0].category}
+              </Typography>
+              <Typography variant='h6' align='right'>
+                الفئة
+              </Typography>
+            </div>
           </div>
           <BottomNavigation className={classes.buttonDiv}>
             <Typography variant='h6' align='right'>
