7f48bbc87ae59d2c642d5180382ed8c0b641b6e1
bayanseder <bayansider88@gmail.com>
2020-07-22 17:01:43 +0300

    add logout icon in header

--- client/src/Components/Common/Header/Header.js
+++ client/src/Components/Common/Header/Header.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import useStyles from './Style';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import TuneIcon from '@material-ui/icons/Tune';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import swal from 'sweetalert';
+
 
 const Header = (props) => {
   const classes = useStyles();
+  const [isLogin, setIsLogin ]=useState(null)
   const { pageName } = props;
+  const history = useHistory()
+
+  useEffect(() => {
+    if(!isLogin){
+      axios.get('/api/user-id')
+        .then(result => {
+          if(!result.data.success){
+            return
+          }
+          setIsLogin(true)
+        })
+        .catch(err => err)
+    }
+  },[isLogin])
+
+  const handelLogOut= ()=> {
+    axios.post('/api/logout')
+    .then(result => history.push('/'))
+    .catch(err => swal({
+      title: 'حدث خطأ أثناء تسجيل الخروج يرجى المحاوله مره اخرى',
+      icon: 'warning',
+    }))
+  }
   return (
     <div className={classes.root}>
       <Toolbar>
@@ -23,7 +52,17 @@ const Header = (props) => {
           >
             <TuneIcon />
           </IconButton>
-        ) : null}
+        ) 
+        : isLogin||pageName!='Search' ?
+        <IconButton
+        edge='start'
+        className={classes.filterButton}
+        aria-label='logout'
+        onClick={()=> handelLogOut()}
+      >
+      <MeetingRoomIcon />
+      </IconButton> 
+      :null}
 
         <IconButton
           edge='start'
